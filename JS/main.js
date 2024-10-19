e350e381df9ff8e2977b19656a0430bc129c363d
Michael_254 <michaelsimiyu54@gmail.com>
2024-10-19 05:40:10 +0300

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// Script to add interactivity
+
+// Select the "Browse Movies" button
+const browseButton = document.querySelector('.cta-button');
+
+// Add a click event listener
+browseButton.addEventListener('click', function() {
+    alert('You clicked the Browse Movies button!');
+});
